b5aebe3c20556c09274d9ced52b1e50c30842305
SomeBodyLite <78022230+SomeBodyLite@users.noreply.github.com>
2021-07-08 23:06:29 +0600

    Попытка номер 2

--- assets/js/path-hover.js
+++ assets/js/path-hover.js
@@ -1,19 +1,21 @@
 
 const colored = document.querySelectorAll('#colored');
 const actived = document.querySelectorAll('.country_block');
-console.log('actived: ', actived);
 
 for (let i = 0; i < colored.length; i++) {
   colored[i].addEventListener('mouseover', () => {
-    colored[i].classList.toggle('active')
+    for (let i = 0; i < actived.length; i++) {
+      actived[i].classList.toggle('active')
+    }
   });
 }
 for (let i = 0; i < colored.length; i++) {
   colored[i].addEventListener('mouseout', () => {
-    colored[i].classList.toggle('active')
+    for (let i = 0; i < actived.length; i++) {
+      actived[i].classList.toggle('active')
+    }
   });
 }
-
 // ---TRY 2--- //
 
 // function tuck() {
